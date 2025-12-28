abf32f6815ad65788cc4eb8b9a60d933e488fb4d
elguesabal <joseelguesabal@gmail.com>
2025-12-28 20:18:24 -0300

    estudando mais sobre websocket refatorando codigo removendo eventos de websocket de testes melhorando e corrigindo bugs no evento de new_message sl mais oq eu fiz, foi tanta coisa q eu to perdido no q eu fiz kkkk

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import config_axios from "./configs/axios.js";
 import config_dotenv from "./configs/dotenv.js";
 
 import connectMongoDB from "./MongoDB/connect.js";
+import { Chat, Message } from "./MongoDB/schema.js";
 
 import verifySignature from "./middleware/verify-signature.js";
 import response from "./middleware/response.js";
@@ -23,32 +24,16 @@ app.post("/webhook", verifySignature, response, verifyProductIndicator, webhookM
 app.post("/message", message);
 app.get("/home", home);
 
-import { Chat, Message } from "./MongoDB/schema.js";
 app.get("/chat", async (req, res) => {
 	const { phone } = req.query;
 
 	if (!phone) return (res.sendStatus(400));
 	const chat = await Message.find({ phone: phone });
 	if (!chat) return (res.sendStatus(404));
-	console.log(chat)
-	// let messages = [];
 	let messages = "";
-	for (const message of chat) {
-		// messages.push(message.text);
-		messages += `<p style="width:100%; margin:0; text-align:${(message.direction === "inbound") ? "left" : "right"};">${message.text}</p>`;
-	}
+	for (const message of chat) messages += `<p style="width:100%; margin:0; text-align:${(message.direction === "inbound") ? "left" : "right"};">${message.text}</p>`;
 	res.status(200).send(messages);
 });
-
-app.get("/contacts", async (req, res) => {
-	res.status(200).send(Array(15).fill({
-		photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT548e7yKxVzd9AoGwcjuciTV94wTtuZPzyC_-kWy3r&s",
-		phone: "+00 (00) 00000-0000",
-		lastMessage: "Last message..."
-	}));
-});
-
-// app.listen(process.env.PORT || 3000, () => console.log("Servidor rodando"));
 
 
 
@@ -66,10 +51,6 @@ const io = new Server(server, {
 
 io.on("connection", (socket) => {
 	// console.log("Cliente conectado:", socket.id);
-
-	// socket.on("message", (data) => {
-	// 	io.emit("message", data);
-	// });
 
 	socket.on("contacts", async (payload, callback) => {
 		callback({
@@ -94,8 +75,14 @@ io.on("connection", (socket) => {
 	});
 
 	setInterval(() => {
-		socket.emit("teste", {
-			message: "testandoooo"
+		socket.emit("new_message", {
+			phone: "5521971178764",
+			lastMessage: {
+				text: "nova mensagem",
+				humanViewed: false,
+				status: "read",
+				timestamp: new Date()
+			}
 		});
 	}, 5000);
 
