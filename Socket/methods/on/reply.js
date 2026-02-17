4bec858090126c6005f3f6331989ff71e3ffaced
elguesabal <joseelguesabal@gmail.com>
2026-02-17 13:42:01 -0300

    corrigindio erro no evento on de websocke em que a data de ultima mensagem de usuario estava sendo comparada de forma errada e invertendo a resposta se a janela ainda estava dentro das 24 horas corrigindo bugs

--- Socket/methods/on/reply.js
+++ Socket/methods/on/reply.js
@@ -15,8 +15,11 @@ export async function replyWindow(socket, data, callback) {
 		const lastDate = new Date(timestamp);
 		const expirationDate = new Date(lastDate.getTime() + 24 * 60 * 60 * 1000);
 
+// console.log("expirationDate:", expirationDate)
+// console.log("date:", (new Date()))
+// console.log(expirationDate < (new Date()))
 setTimeout(() => {
-		callback(expirationDate < (new Date()));
+		callback(expirationDate > (new Date()));
 }, 1000);
 	} catch (error) {
 		await mongodb.saveError(idPhone, `Error no metodo "replyWindow": ${error}`);
