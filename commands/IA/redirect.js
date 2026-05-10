666c650d78795d6d8f929a5d4d57102e84694021
elguesabal <joseelguesabal@gmail.com>
2026-05-10 11:42:58 -0300

    fazendo pequeno ajuste no redirect para se adequar a fila de espera do atendimento humano

--- commands/IA/redirect.js
+++ commands/IA/redirect.js
@@ -9,15 +9,14 @@ import send from "../../Send/Send.js";
 */
 export default async function redirect(account, phone) {
 	try {
-		const contact = account.bot.redirect?.numbers[0];
-
-		if (!contact) return ;
 		await mongodb.saveHumanService(account.idPhone, phone);
-		if (account.bot.redirect.numbers.length > 1) await mongodb.updateRedirect(account.idPhone);
-		await send.text(account, phone, { text: { body: `Em breve um atendente vai entrar em contato com você.\nLink do contato: https://wa.me/${contact}?text=Gostaria%20de%20continuar%20o%20atendimento` } });
+		if (account.bot.redirect.numbers.length > 0) {
+			if (account.bot.redirect.numbers.length > 1) await mongodb.updateRedirect(account.idPhone);
+			await send.text(account, phone, { text: { body: `Em breve um atendente vai entrar em contato com você.\nLink do contato: https://wa.me/${account.bot.redirect?.numbers[0]}?text=Gostaria%20de%20continuar%20o%20atendimento` } });
+			await send.text(account, account.bot.redirect?.numbers[0], { text: { body: `Um cliente deseja atendimento humano.\nContato: ${phone}\nLink do contato: https://wa.me/${phone}` } });
+			await send.text(account, account.bot.redirect?.numbers[0], { text: { body: `Link do hitórico de conversa: ${process.env.URL_FRONT_END}/chat/${phone}` } });
+		}
 		if (account.bot.redirect.message) await send.text(account, phone, { text: { body: account.bot.redirect.message } });
-		await send.text(account, contact, { text: { body: `Um cliente deseja atendimento humano.\nContato: ${phone}\nLink do contato: https://wa.me/${phone}` } });
-		await send.text(account, contact, { text: { body: `Link do hitórico de conversa: ${process.env.URL_FRONT_END}/chat/${phone}` } });
 		await mongodb.saveMetricRedirect(account.idPhone);
 	} catch (error) {
 		await mongodb.saveError(account.idPhone, `Error na funcao "redirect": ${error}`);
